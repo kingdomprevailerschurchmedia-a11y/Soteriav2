b02e22e11a0d47f71ed0ced814a1eba5a5888d57
Joseph <kingdomprevailerschurch.media@gmail.com>
2026-08-15 05:33:09 +0100

    Story 10.10 completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,9 +28,16 @@ android {
             }
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 }
 
