ac699aa776479f251b82903ffdf8087d923ddc49
Joseph <kingdomprevailerschurch.media@gmail.com>
2026-09-17 17:22:59 +0100

    17th September 2026 dashboard changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,10 @@ allprojects {
     }
 }
 
+rootProject.layout.buildDirectory.set(file("../build"))
+
 subprojects {
+    layout.buildDirectory.set(rootProject.layout.buildDirectory.dir(project.name))
     if (project.name != "app") {
         project.evaluationDependsOn(":app")
     }
